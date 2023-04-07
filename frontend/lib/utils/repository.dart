0224be91a24ed3f4bfe897b6f12c371acc234847
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-04-07 23:28:38 +0200

    Improvements

--- frontend/lib/utils/repository.dart
+++ frontend/lib/utils/repository.dart
@@ -75,6 +75,25 @@ class Repository {
         'status': Status.accepted.name,
       });
 
+  static Future accept(Task task) {
+    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+    final List<String> assignedTo = task.assignedTo;
+    assignedTo.add(userId);
+
+    final Map<String, dynamic> assignedInfo = task.assignedInfo;
+    assignedInfo[userId] = {
+      'avatar': FirebaseAuth.instance.currentUser?.photoURL,
+      'email': FirebaseAuth.instance.currentUser?.email,
+      'name': FirebaseAuth.instance.currentUser?.displayName,
+    };
+
+    return _collection().doc(task.id).update({
+      Task.FIELD_ASSIGNED_TO: assignedTo,
+      Task.FIELD_ASSIGNED_INFO: assignedInfo,
+    });
+  }
+
   static Future delete(Task task) => _collection().doc(task.id).delete();
 
   static CollectionReference<Map<String, dynamic>> _collection() => FirebaseFirestore.instance.collection('tasks');
