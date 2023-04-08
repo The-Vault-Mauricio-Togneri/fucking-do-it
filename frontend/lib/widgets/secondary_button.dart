5be2a89b28c4e8baae5f79d0501078d01a11cb99
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-04-08 21:04:49 +0200

    Improvements

--- frontend/lib/widgets/secondary_button.dart
+++ frontend/lib/widgets/secondary_button.dart
@@ -40,16 +40,19 @@ class SecondaryButton extends StatelessWidget {
           shape: const StadiumBorder(),
         ),
         onPressed: onSubmit,
-        child: (icon != null)
-            ? Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  icon!,
-                  const HBox(10),
-                  Label(text: text, color: textColor),
-                ],
-              )
-            : Label(text: text, color: textColor),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 2),
+          child: (icon != null)
+              ? Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    icon!,
+                    const HBox(10),
+                    Label(text: text, color: textColor),
+                  ],
+                )
+              : Label(text: text, color: textColor),
+        ),
       ),
     );
   }
