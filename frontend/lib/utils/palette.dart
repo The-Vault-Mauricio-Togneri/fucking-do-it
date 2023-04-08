02b88862b963bd3f50e14fa4e42c358c5de4dafe
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-04-08 20:48:09 +0200

    Improvements

--- frontend/lib/utils/palette.dart
+++ frontend/lib/utils/palette.dart
@@ -7,7 +7,7 @@ class Palette {
   static const Color white = Colors.white;
   static const Color grey = Colors.grey;
   static const Color error = Color(0xffef5365);
-  static const Color success = Color(0xff5b8644);
+  static const Color success = Color(0xff82b368);
   static const Color background = Color(0xffedf0fc);
   static const Color border = Color(0xffdadada);
   static const Color unselected = Color(0xffdddddd);
